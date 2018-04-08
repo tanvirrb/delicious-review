97b81970b37652292390c980c255b202af3fadda
Tanvir Islam <tanvir@Tanvirs-MacBook-Pro.local>
2018-04-08 17:09:02 +0600

    example env file added

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -86,7 +86,7 @@ exports.updateStore = async (req, res) => {
         new: true,
         runValidators: true,
     }).exec();
-    req.flash('success', `Successfully updated <strong>${store.name}</strong>. <a href="/stores/${store.slug}"> View Store</a>}`);
+    req.flash('success', `Successfully updated <strong>${store.name}</strong>. <a href="/stores/${store.slug}"> View Store</a>`);
     res.redirect(`/stores/${store._id}/edit`);
 };
 
